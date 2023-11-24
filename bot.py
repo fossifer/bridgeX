8152bcaa80f0b5e20bca845b8c8f8966a787419f
fossifer <23732689+fossifer@users.noreply.github.com>
2023-11-23 23:21:47 -0600

    Insert messages to MongoDB and implemented synchronous message deletion

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@ import asyncio
 import bottom
 import discord
 import logging
+from collections import defaultdict
 from config import Config
+from datetime import datetime
 from message import Message
 from motor.motor_asyncio import AsyncIOMotorClient
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, errors
 
 # Load configs from file
 config = Config('bridge.yaml')
@@ -67,12 +69,12 @@ async def on_ready():
     logging.info(f'Discord: we have logged in as {dc_bot.user}')
 
 # Initialize MongoDB client
-# mongo_client = AsyncIOMotorClient(config.get_nowait('Mongo', 'uri'))
-# db = mongo_client[config.get_nowait('Mongo', 'database_name')]
-# messages_collection = db[config.get_nowait('Mongo', 'collection_name')]
+mongo_client = AsyncIOMotorClient(config.get_nowait('Mongo', 'uri'))
+db = mongo_client[config.get_nowait('Mongo', 'database_name')]
+msg_collection = db[config.get_nowait('Mongo', 'collection_name')]
 
 # Initialize the asyncio.Queue
-message_queue = asyncio.Queue()
+message_queue: asyncio.Queue[Message] = asyncio.Queue()
 
 
 @irc_bot.on('CLIENT_CONNECT')
@@ -114,6 +116,9 @@ async def message(nick, target, message, **kwargs):
     IRC listener serves as a producer to add new messages to queue.
     """
     # kwargs keys: user, host
+    # IRC does not send us the time when a message is received
+    # so just use the current UTC time
+    received_at = datetime.utcnow()
     mynick = await config.get('IRC', 'nick')
     # Don't echo self
     if nick == mynick:
@@ -124,8 +129,10 @@ async def message(nick, target, message, **kwargs):
 
     await message_queue.put(await Message.create({
         'group': target,
+        'host': kwargs.get('host', ''),
         'nick': nick,
         'text': message,
+        'created_at': received_at,
     }))
 
 @tg_bot.on(events.NewMessage(incoming=True))
@@ -138,6 +145,33 @@ async def telegram_listener(event):
         return
     logger.info(f'Telegram {event.chat_id} incoming message: ' + str(event.message))
     await message_queue.put(await Message.create(event.message))
+
+@tg_bot.on(events.MessageDeleted)
+async def telegram_deleted_listener(event):
+    """
+    Telegram listener that detects when messages are deleted.
+    From telethon doc it isn’t 100% reliable. Actually it works like only 1% of the time.
+    So we have to implement the polling method as well ¯\_(ツ)_/¯
+    """
+    if ('telegram/' + str(event.chat_id)) not in bridge_map:
+        return
+    logger.info(f'Telegram message {event.deleted_ids} were deleted in {event.chat_id}')
+    old_msg, to_delete = defaultdict(list)
+    for msg_id in event.deleted_ids:
+        next_to_delete = await find_bridged_messages_and_update('telegram', event.chat_id, msg_id, None, {
+            'deleted': True,
+            'deleted_at': datetime.utcnow(),
+        })
+        # Do not re-delete a message
+        if old_msg.get('deleted'):
+            continue
+        # Merge two default dicts
+        for key in next_to_delete:
+            to_delete[key].extend(next_to_delete[key])
+    if not to_delete:
+        return
+    logger.info(f'Messages to be deleted in bridged groups: {to_delete}')
+    await message_queue.put({'action': 'delete', 'body': to_delete})
 
 @dc_bot.event
 async def on_message(message):
@@ -152,6 +186,68 @@ async def on_message(message):
     logger.info(f'Discord {message.channel.id} incoming message: ' + str(message))
     await message_queue.put(await Message.create(message))
 
+@dc_bot.event
+async def on_message_delete(message):
+    """
+    Discord listener that detects when messages are deleted. One at a time.
+    I am glad that it is much more reliable than the telegram listener.
+    """
+    if ('discord/' + str(message.channel.id)) not in bridge_map:
+        return
+    logger.info(f'Discord message {message.id} were deleted in {message.channel.id}')
+    old_msg, to_delete = await find_bridged_messages_and_update('discord', message.channel.id, message.id, message.author == dc_bot.user, {
+        'deleted': True,
+        'deleted_at': datetime.utcnow(),
+    })
+    # Do not re-delete a message
+    if old_msg.get('deleted') or not to_delete:
+        return
+    logger.info(f'Messages to be deleted in bridged groups: {to_delete}')
+    await message_queue.put({'action': 'delete', 'body': to_delete})
+
+async def find_bridged_messages_and_update(platform: str, group_id, message_id: int, self_authored, update={}):
+    """
+    Find all relayed messages connected with given group id and message id from MongoDB.
+
+    platform: discord, telegram, etc. All in lower case.
+    self_authored: True if the given message is authored by self (the bridge bot), False otherwise, None if unknown (will try both)
+    update: a dict with fields to be updated. Do not include MongoDB keywords like $set.
+    Returns the original message and a defaultdict, key is groups in the form platform/groupid, value is list of message ids
+    """
+    ret = defaultdict(list)
+    if self_authored:
+        # It's a relayed message originated from other platforms
+        message = await msg_collection.find_one_and_update({
+            'to_groups': f'{platform}/{group_id}',
+            'to_message_ids': message_id,
+        }, {'$set': update})
+        if message:
+            # Return messages in to_groups, plus from_group, minus current group
+            to_groups = message.get('to_groups', [])
+            to_message_ids = message.get('to_message_ids', [])
+            for i in range(len(to_groups)):
+                if to_message_ids[i] and to_message_ids[i] != message_id:
+                    ret[to_groups[i]].append(to_message_ids[i])
+            ret[f'{message.get("from_platform")}/{message.get("from_group")}'].append(message.get('from_message_id'))
+    else:
+        # It's originated from the current group
+        message = await msg_collection.find_one_and_update({
+            'from_platform': 'discord',
+            'from_group': str(group_id),
+            'from_message_id': message_id,
+        }, {'$set': update})
+        if message:
+            # Pack the messages to delete and send it to queue
+            to_groups = message.get('to_groups', [])
+            to_message_ids = message.get('to_message_ids', [])
+            for i in range(len(to_groups)):
+                if to_message_ids[i]:
+                    ret[to_groups[i]].append(to_message_ids[i])
+        if not message and self_authored is None:
+            # Try again assuming it is authored by bridge bot
+            return await find_bridged_messages_and_update(platform, group_id, message_id, True, update)
+    return message, ret
+
 async def worker():
     """
     The consumer will fetch messages from queue, insert into database and
@@ -159,28 +255,93 @@ async def worker():
     """
     while True:
         message = await message_queue.get()
+        if type(message) is dict:
+            # internal message, indicates to delete or update existing messages
+            logger.info(f'internal message: {message}')
+            action = message.get('action')
+            if action == 'delete':
+                for group_to_delete, ids_to_delete in message.get('body', {}).items():
+                    platform, group_id = group_to_delete.split('/', 1)
+                    if platform == 'irc':
+                        # Cannot delete IRC messages
+                        continue
+                    elif platform == 'telegram':
+                        # Delete all messages at the same time
+                        try:
+                            await tg_bot.delete_messages(int(group_id), ids_to_delete)
+                        except (errors.ChannelInvalidError, errors.ChannelPrivateError, errors.MessageDeleteForbiddenError) as e:
+                            logger.warning(f'Telegram error occured on deleting messages {ids_to_delete} in {group_id}: {e}')
+                        except Exception as e:
+                            # TODO: probably catch errors.FloodWaitError
+                            logger.warning(f'Unknown error occured on deleting messages {ids_to_delete} in {group_id}: {e}')
+                    elif platform == 'discord':
+                        # Have to delete messages one by one
+                        channel = dc_bot.get_channel(int(group_id))
+                        if not channel:
+                            # TODO: log error
+                            continue
+                        for msg_id in ids_to_delete:
+                            msg = await channel.fetch_message(msg_id)
+                            try:
+                                await msg.delete()
+                            except discord.errors as e:
+                                logger.warning(f'Discord error occured on deleting message {msg_id} in {group_id}: {e}')
+                            except Exception as e:
+                                logger.warning(f'Unknown error occured on deleting message {msg_id} in {group_id}: {e}')
+                    else:
+                        logger.warning(f'Unknown platform: {platform} (from {message}), please report this bug')
+            elif action == 'edit':
+                pass
+            else:
+                logger.warning(f'Unknown action {action} from message of a listener: {message}')
+            continue
         logger.info(f'outgoing message: ' + str(message))
-        from_group = message.from_prefix + message.from_group
+        from_group = message.from_platform + '/' + message.from_group
         relay_message_text = f'[{message.platform_prefix} - {message.from_nick}] {message.text}'
+        to_msg_ids = []
+        # TODO: this should be dynamically loaded from config
         for group_to_send in bridge_map.get(from_group, []):
             # TODO: make this a helper function
             platform, group_id = group_to_send.split('/', 1)
             if platform == 'irc':
                 # TODO: if message is too long, send in multiple times
                 irc_bot.send('PRIVMSG', target=group_id, message=relay_message_text)
+                to_msg_ids.append(None)  # Unable to track IRC messages
+                logger.info(f'sent message to {group_to_send}')
             elif platform == 'telegram':
                 # Telethon only accept int group ids
-                await tg_bot.send_message(int(group_id), relay_message_text, parse_mode='md')
+                sent = await tg_bot.send_message(int(group_id), relay_message_text, parse_mode='md')
+                to_msg_ids.append(sent.id)
+                logger.info(f'sent message to {group_to_send}, msg id = {sent.id}')
             elif platform == 'discord':
                 # Discord only accept int group ids
                 channel = dc_bot.get_channel(int(group_id))
                 if not channel:
                     # TODO: log error
                     continue
-                await channel.send(relay_message_text)
+                sent = await channel.send(relay_message_text)
+                to_msg_ids.append(sent.id)
+                logger.info(f'sent message to {group_to_send}, msg id = {sent.id}')
             else:
                 # Unknown platform
-                pass
+                to_msg_ids.append(None)
+                logger.warning(f'Unknown platform: {platform} (from {group_to_send}), check your Bridge config')
+
+        await msg_collection.insert_one({
+            'deleted': False,
+            'created_at': message.created_at,
+            'edited_at': None,
+            'deleted_at': None,
+            'from_platform': message.from_platform,
+            'from_group': message.from_group,
+            'from_user_id': message.from_user_id,
+            'from_nick': message.from_nick,
+            'from_message_id': message.from_message_id,
+            'to_groups': bridge_map.get(from_group, []),
+            'to_message_ids': to_msg_ids,
+            'text': message.text,
+            'embeds': [],
+        })
 
 async def main():
     await asyncio.gather(
--- message.py
+++ message.py
@@ -25,11 +25,17 @@ class Message:
     """
 
     def __init__(self):
+        self.deleted = False
         self.text = None
+        self.from_user_id = None
         self.from_nick = None
         self.from_group = None
-        self.from_prefix = None
+        self.from_message_id = None
+        self.from_platform = None
         self.platform_prefix = None
+        self.created_at = None
+        self.edited_at = None
+        self.deleted_at = None
 
     @classmethod
     async def create(cls, message):
@@ -42,31 +48,40 @@ class Message:
         if type(message) is telethon.tl.types.Message:
             # Telegram message
             self.text = message.text
-            # TODO: anonymous sender
+            # TODO: Make nickname of anonymous sender configurable
+            self.from_user_id = message.sender_id
             self.from_nick = (await get_tg_nick(message.sender)) or 'Anonymous'
             self.from_group = str(message.chat_id)
-            self.from_prefix = 'telegram/'
+            self.from_message_id = message.id
+            self.from_platform = 'telegram'
             self.platform_prefix = await config.get('Telegram', 'platform_prefix', default='T')
+            self.created_at = message.date
         elif type(message) is discord.Message:
             # Discord message
             self.text = message.content
+            self.from_user_id = message.author.id
             if (await config.get('Discord', 'nick_style', default='nickname')) == 'nickname':
                 self.from_nick = message.author.display_name
             else:
                 self.from_nick = message.author.name
             self.from_group = str(message.channel.id)
-            self.from_prefix = 'discord/'
+            self.from_message_id = message.id
+            self.from_platform = 'discord'
             self.platform_prefix = await config.get('Discord', 'platform_prefix', default='D')
+            self.created_at = message.created_at
         elif type(message) is dict:
             # IRC message
             self.text = message.get('text', '')
+            self.from_user_id = message.get('host', '')
             self.from_nick = message.get('nick', '')
             self.from_group = message.get('group', '')
-            self.from_prefix = 'irc/'
+            self.from_message_id = None  # IRC does not have message ids
+            self.from_platform = 'irc'
             self.platform_prefix = await config.get('IRC', 'platform_prefix', default='I')
+            self.created_at = message.get('created_at')
         else:
             raise TypeError('Unknown message type')
         return self
 
     def __str__(self):
-        return f'{self.from_prefix}{self.from_group} -> [{self.platform_prefix} - {self.from_nick}] {self.text}'
+        return f'[{self.created_at.isoformat()}] {self.from_platform}/{self.from_group}:{self.from_message_id} -> [{self.platform_prefix} - {self.from_nick} ({self.from_user_id})] {self.text}'
