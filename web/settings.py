ec1238fa6234895e1f8d9568b5ec88925a3f0b9e
fossifer <23732689+fossifer@users.noreply.github.com>
2024-01-10 03:46:41 -0600

    Add static files serving

--- web/settings.py
+++ web/settings.py
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'files/'
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'files']
+else:
+    STATIC_ROOT = BASE_DIR / 'files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- web/urls.py
+++ web/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -21,4 +23,4 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
